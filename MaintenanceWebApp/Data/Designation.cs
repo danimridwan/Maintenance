90e39c4f6c1552bb47d5af10d8b0ce10e89af9e5
daniridwan <dani.ridwan@redeco-rpu.com>
2025-04-17 11:38:15 +0700

    Initial

---
+++ MaintenanceWebApp/Data/Designation.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaintenanceWebApp.Data
+{
+    public class Designation
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string DesignationName { get; set; }
+    }
+}
---
+++ MaintenanceWebApp/Migrations/20231221073626_AddEmployeeAndDesignationModels.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MaintenanceWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmployeeAndDesignationModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Designations",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DesignationName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Designations", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DesignationID = table.Column<int>(type: "int", nullable: false),
+                    BasicSalary = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    PresentAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PermanentAddress = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Employees_Designations_DesignationID",
+                        column: x => x.DesignationID,
+                        principalTable: "Designations",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DesignationID",
+                table: "Employees",
+                column: "DesignationID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Designations");
+        }
+    }
+}
