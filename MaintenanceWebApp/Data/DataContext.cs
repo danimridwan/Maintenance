69f7ee57b264a5f3f2d1e73e9e48700853242e49
daniridwan <dani.ridwan@redeco-rpu.com>
2025-05-21 11:03:14 +0700

    Add Inventory Tables

--- MaintenanceWebApp/Data/DataContext.cs
+++ MaintenanceWebApp/Data/DataContext.cs
@@ -10,6 +10,18 @@ namespace MaintenanceWebApp.Data
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Designation> Designations { get; set; }
         public DbSet<MaintenanceTask> MaintenanceTasks { get; set; }
+        //Inventory
+        public DbSet<Tank> Tanks { get; set; }
+        public DbSet<Inventory> Inventories { get; set; }
+        public DbSet<BreatherValve> BreatherValves { get; set; }
+        public DbSet<PRValveTank> PRValveTanks { get; set; }
+        public DbSet<PRValvePump> PRValvePumps { get; set; }
+        public DbSet<PCValveTank> PCValveTanks { get; set; }
+        public DbSet<Valve> Valves { get; set; }
+        public DbSet<Pump> Pumps { get; set; }
+        public DbSet<FlowMeter> FlowMeters { get; set; }
+        public DbSet<Panel> Panels { get; set; }
+        public DbSet<IT> ITs { get; set; }
 
         public DataContext(DbContextOptions options) : base(options)
         {
---
+++ MaintenanceWebApp/Data/Tank.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
+namespace MaintenanceWebApp.Data
+{
+    public class Tank
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TankID { get; set; }
+
+        public string TankNo { get; set; }
+
+        public int Capacity { get; set; }
+
+        public string InternalCoating { get; set; }
+
+    }
+}
