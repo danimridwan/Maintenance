2ecc88675143965b3cee744ee63520b0f71b3484
LuizZ808 <darlucky65@gmail.com>
2025-08-04 14:40:38 +0700

    Update _Host #2

---
+++ MaintenanceWebApp/wwwroot/js/blazor-interop.js
@@ -0,0 +1,19 @@
+﻿// Pastikan objek 'bootstrap' dari Bootstrap 5 JS sudah dimuat!
+window.initBootstrapTabs = function() {
+    console.log("Custom JS: initBootstrapTabs dipanggil!");
+    var tabButtons = document.querySelectorAll('button[data-bs-toggle="tab"]');
+    tabButtons.forEach(function(button) {
+    });
+
+    // Contoh: Mengaktifkan tab pertama secara default
+    var firstTabButton = document.querySelector('.nav-tabs button[data-bs-toggle="tab"]:first-child');
+    if (firstTabButton && !firstTabButton.classList.contains('active')) {
+        var firstTab = new bootstrap.Tab(firstTabButton);
+        firstTab.show();
+    }
+};
+
+// Contoh fungsi JS Interop lainnya jika Anda punya
+window.myCustomAlert = (message) => {
+    alert(message);
+};
