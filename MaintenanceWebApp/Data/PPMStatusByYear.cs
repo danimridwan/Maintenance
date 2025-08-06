face589ca15ad8e0214605c003093a0b86a1cefe
danimridwan <danimr90@gmail.com>
2025-08-06 14:34:22 +0700

    PPM Performance pie chart

--- MaintenanceWebApp/Data/PPMStatusByYear.cs
+++ MaintenanceWebApp/Data/PPMStatusByYear.cs
@@ -6,7 +6,7 @@ namespace MaintenanceWebApp.Data
     public class PPMStatusByYear
     {
         [Key]
-        public int RowNum { get; set; }
+        public long RowNum { get; set; }
         public int Tasks { get; set; }
         public int Year { get; set; }
         public string Status { get; set; }
